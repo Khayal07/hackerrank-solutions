8f28952fefad3244198569c1077ad7628ac009d3
Khayal <khayal.izada39@gmail.com>
2026-09-02 03:54:42 +0400

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 7884-python-division-Hackerrank/7884-python-division.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    x = a // b
+    y = a / b
+    
+    print(x)
+    print(y)
+    
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
