93ee7e5f577e57932e03ed70bfc7e8d219f78fe8
Khayal <khayal.izada39@gmail.com>
2026-09-02 03:50:20 +0400

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 7883-arithmetic-operators-Hackerrank/7883-arithmetic-operators.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    sum_two = a + b
+    difference = a - b
+    product = a * b
+    
+    print(sum_two)
+    print(difference)
+    print(product)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
