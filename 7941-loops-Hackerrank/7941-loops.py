a1325991214c8c5e5cb019b6fda0012237672d40
Khayal <khayal.izada39@gmail.com>
2026-09-02 03:57:20 +0400

    Force committed solution - Time: O(n), Space: O(1)

---
+++ 7941-loops-Hackerrank/7941-loops.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+    i = 0
+    while i < n:
+        print(i ** 2)
+        
+        i += 1
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
