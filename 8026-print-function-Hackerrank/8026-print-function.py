005b642deda64228835aa973adb522fed69fc487
Khayal <khayal.izada39@gmail.com>
2026-09-02 04:07:32 +0400

    Force committed solution

---
+++ 8026-print-function-Hackerrank/8026-print-function.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+    s = ""
+    i = 1
+    while i <= n:
+        s = s + str(i)
+        i += 1
+        
+    print(s)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
