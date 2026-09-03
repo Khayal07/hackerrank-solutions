6dd843a06f7d7d5e9e8788b5ba332d8530341577
Khayal <khayal.izada39@gmail.com>
2026-09-03 13:20:50 +0400

    Force committed solution - Time: O(n), Space: O(n)

---
+++ 7890-mutations-Hackerrank/7890-mutations.py
@@ -0,0 +1,11 @@
+def mutate_string(string, position, character):
+    l = list(string)
+    l[position] = character
+    string = ''.join(l)
+    return string
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
