53ea525ced79154ed9ebf9a4916625d7ee57f44b
Khayal <khayal.izada39@gmail.com>
2026-09-03 23:33:05 +0400

    Force committed solution - Time: O(n), Space: O(n)

---
+++ 14254-capitalize-Hackerrank/14254-capitalize.py
@@ -0,0 +1,17 @@
+
+
+# Complete the solve function below.
+def solve(s):
+    lst = []
+    a = s.split(' ')
+    for i in a:
+        lst.append(i.capitalize())
+    
+    return ' '.join(lst)
+    
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
