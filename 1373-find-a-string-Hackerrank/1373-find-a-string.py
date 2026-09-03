fc978626ed3b12c2aff83dd7693a04c0f68a88fb
Khayal <khayal.izada39@gmail.com>
2026-09-03 13:39:00 +0400

    Force committed solution - Time: O(len(string) * len(sub_string)), Space: O(len(sub_string))

---
+++ 1373-find-a-string-Hackerrank/1373-find-a-string.py
@@ -0,0 +1,13 @@
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(0, len(string)):
+        if string[i : i + len(sub_string)] == sub_string:
+            count += 1
+        
+    return count
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
