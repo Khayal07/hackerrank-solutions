62241d521461a0b5938a718f3626049e5599c78e
Khayal <khayal.izada39@gmail.com>
2026-09-04 18:03:06 +0400

    Force committed solution - Time: O(n), Space: O(n)

---
+++ 9375-introduction-to-sets-Hackerrank/9375-introduction-to-sets.py
@@ -0,0 +1,9 @@
+def average(array):
+    distinct_elems = set(array)
+    return round(sum(distinct_elems) / len(distinct_elems), 3)
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
