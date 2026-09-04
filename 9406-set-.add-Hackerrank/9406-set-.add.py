b224111ae46766b01be50bf735219f5c3e81322a
Khayal <khayal.izada39@gmail.com>
2026-09-04 18:31:16 +0400

    Force committed solution - Time: O(N), Space: O(N)

---
+++ 9406-set-.add-Hackerrank/9406-set-.add.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    N = int(input())
+    S = set()
+    
+    for _ in range(N):
+        S.add(input())
+    
+    print(len(S))
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
