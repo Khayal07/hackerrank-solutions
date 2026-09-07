f00e626a9a45c028feedf252befda01952230a59
Khayal <khayal.izada39@gmail.com>
2026-09-07 14:53:47 +0400

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 9951-polar-coordinates-Hackerrank/9951-polar-coordinates.py
@@ -0,0 +1,12 @@
+import cmath
+
+if __name__ == '__main__':
+    z = complex(input())
+    
+    print(abs(z))
+    print(cmath.phase(z))
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
