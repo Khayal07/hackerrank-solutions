68682e93815e50b82c0e87015d7cbfba42da6584
Khayal <khayal.izada39@gmail.com>
2026-09-07 15:21:31 +0400

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 7882-integers-come-in-all-sizes-Hackerrank/7882-integers-come-in-all-sizes.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+    
+    print(pow(a, b) + pow(c, d))
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
