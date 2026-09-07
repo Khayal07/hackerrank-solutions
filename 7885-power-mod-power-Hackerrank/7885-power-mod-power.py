4d0d32ff10efe68f05f4a9211842fc5a5d672f7e
Khayal <khayal.izada39@gmail.com>
2026-09-07 15:10:47 +0400

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 7885-power-mod-power-Hackerrank/7885-power-mod-power.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+m = int(input())
+
+print(pow(a, b))
+print(pow(a, b, m))
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
