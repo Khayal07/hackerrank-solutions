bf77bfd383067dcc177747d7da2c45f64b683ff4
Khayal <khayal.izada39@gmail.com>
2026-09-07 15:32:39 +0400

    Force committed solution - Time: O(n^2), Space: O(1)

---
+++ 7944-triangle-quest-Hackerrank/7944-triangle-quest.py
@@ -0,0 +1,7 @@
+for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(((10**i - 1) // 9) * i)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
