3da3567956604433f05fdd8fb9de72fb0763e8de
Khayal <khayal.izada39@gmail.com>
2026-09-10 18:51:14 +0400

    Force committed solution - Time: O(n), Space: O(1)

---
+++ 8212-time-delta-Hackerrank/8212-time-delta.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+from datetime import datetime
+
+# Complete the time_delta function below.
+def time_delta(t1, t2):
+    fmt = "%a %d %b %Y %H:%M:%S %z"
+    dt1 = datetime.strptime(t1, fmt)
+    dt2 = datetime.strptime(t2, fmt)
+    
+    return str(int(abs((dt1 - dt2).total_seconds())))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        t1 = input()
+
+        t2 = input()
+
+        delta = time_delta(t1, t2)
+
+        fptr.write(delta + '\n')
+
+    fptr.close()
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
