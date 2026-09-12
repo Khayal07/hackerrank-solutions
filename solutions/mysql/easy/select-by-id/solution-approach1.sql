37d2d569bb687817ef80a229253887e3ea474e4b
Khayal <khayal.izada39@gmail.com>
2026-09-12 17:03:57 +0400

    ✅ Select By ID [mysql] — approach1

---
+++ solutions/mysql/easy/select-by-id/solution-approach1.sql
@@ -0,0 +1,13 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+-- Problem     Select By ID
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-12, 05:03 p.m.
+-- ──────────────────────────────────────────────────
+
+select * from city
+where id = 1661;
