e0064229883f1d6384a2053510c8026e2df4d5f8
Khayal <khayal.izada39@gmail.com>
2026-09-14 01:54:01 +0400

    ✅ Input() [python3] — approach1

---
+++ solutions/python3/easy/input/solution-approach1.py
@@ -0,0 +1,13 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/input/problem?isFullScreen=true
+# Problem     Input()
+# Difficulty  Easy
+# Subdomain   Built-Ins
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-14, 01:53 a.m.
+# ──────────────────────────────────────────────────
+
+x, k = map(int, input().split())
+print(eval(input()) == k)
